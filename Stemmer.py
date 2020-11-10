4187f88a1c6c06e835a8487199553822c635d558
Towhid Ahmed Foysal <towhidfoysal123@gmail.com>
2020-11-10 23:31:48 +0600

    v1.3

--- Stemmer.py
+++ Stemmer.py
@@ -1,11 +1,23 @@
 from bn_nlp.preprocessing import ban_processing,StaticArray
 from bn_nlp.tokenizer import wordTokenizer
-from bnltk.stemmer import BanglaStemmer
-bn_stemmer = BanglaStemmer()
+import functools
+import re
+import string
 wordtokens=wordTokenizer()
 word_vec=[]
 word_dict={}
 bp=ban_processing()
+rule_words = ['ই', 'ও', 'তো', 'কে', 'তে', 'রা', 'চ্ছি', 'চ্ছিল', 'চ্ছে', 'চ্ছিস', 'চ্ছিলেন', 'চ্ছ', 'য়েছে', 'েছ', 'েছে',
+              'েছেন', 'রছ', 'রব', 'েল', 'েলো', 'ওয়া', 'েয়ে', 'য়', 'য়ে', 'েয়েছিল', 'েছিল', 'য়েছিল', 'েয়েছিলেন',
+              'ে.েছিলেন', 'েছিলেন', 'লেন', 'দের', 'ে.ে', 'ের', 'ার', 'েন', 'বেন', 'িস', 'ছিস', 'ছিলি', 'ছি', 'ছে', 'লি',
+              'বি', 'ে', 'টি', 'টির', 'েরটা', 'েরটার', 'টা', 'টার', 'গুলো', 'গুলোর', 'েরগুলো', 'েরগুলোর'
+              'যোগ্য','কেই','েও','সহ','রা','ভাবে','কারি','কৃত','ই','কে','র','কি','েই','ভাবে','গুলো',
+              'তে','েতে','গন','মুলক','সুচক','টুকু','টুকুই','গুলি','পদ','সমুহ','সংক্রান্ত','সংলগ্ন','সংশ্লিষ্ট',
+              'সুত্রে','রুপে','ানুসারে','ানুযায়ি','তত্ত্','ি','মুখি','প্রতি','ভাবে'
+              ]
+rule_dict = {"রছ":"র","রব":"র","েয়ে":"া","েয়েছিল":"া","েয়েছিলেন":"া","ে.েছিলেন":"া.","ে.ে":"া."}
+
+
 class stemmerOP:
     def __init__(self):
         for word in open("bn_nlp/word2.txt", "r"):
@@ -24,21 +36,36 @@ class stemmerOP:
         if g in StaticArray.bn2enSuffix:
             word=word[:-1]
         return word
+    def bnCompare(self,item1,item2):
+        return (len(item1)>len(item2))-(len(item1)<len(item2))
 
     def stem(self,word):
-        new_word=bn_stemmer.stem(word)
-        if new_word==word:
-            new_word = self.stem_normalize(word)
-            if self.search(new_word):
-                return new_word
-        else:
-            if self.search(new_word):
-                return new_word
-            new_word= self.stem_normalize(word)
-            if self.search(new_word):
+        suf_arr=[]
+        for wd in rule_words:
+            if re.search('.*' + wd + '$', word):
+                suf_arr.append(wd)
+        suf_arr = sorted(suf_arr, key=functools.cmp_to_key(self.bnCompare))
+
+        if len(suf_arr)>0:
+            for i in suf_arr:
+                if i in rule_dict:
+                    ind = len(word) - len(i)
+                    new_word=word[0:ind]+rule_dict[i]
+                    if self.search(new_word):
+                        return new_word
+                else:
+                    ind = len(word) - len(i)
+                    new_word = word[0:ind]
+                    if len(new_word)==0:
+                        return word
+                    if self.search(new_word):
+                        return new_word
+
+
+        new_word= self.stem_normalize(word)
+        if self.search(new_word):
                 return new_word
         return word
-
     def stemSent(self,sent):
 
         tokens=wordtokens.normalize_tokenizer(sent)
@@ -48,7 +75,3 @@ class stemmerOP:
         result = ' '.join(temp_tokens)
 
         return result
-
-
-
-
